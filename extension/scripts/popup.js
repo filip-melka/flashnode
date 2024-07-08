36d4a849008478a58db9af797690b24603c7b825
filip-melka <melka.filip@proton.me>
2024-07-08 17:08:09 +0200

    feat: add 'save toggle' button

--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -13,6 +13,10 @@ closeBtn.addEventListener("click", () => window.close())
 const progressBar = document.getElementById("bar")
 const flashcard = document.getElementById("flashcard")
 const nextBtn = document.getElementById("btn-next")
+const saveContainer = document.getElementById("save-container")
+const saveToggle = document.getElementById("save-btn")
+const saveStatus = document.getElementById("save-status")
+const summaryMsg = document.getElementById("summary-message")
 
 const tabs = {
     loader: document.getElementById("loader-tab"),
@@ -35,12 +39,17 @@ async function main(url) {
 
         // fetch flashcards
         flashcardsSet = await fetchFlashcards(markdown, url, title)
-        nextFlashcard()
-        changeTab(tabs.flashcards)
+        displayFlashcards()
     } catch (error) {
         console.log(error)
         showError()
     }
+}
+
+function displayFlashcards() {
+    saveContainer.classList.remove("hidden")
+    nextFlashcard()
+    changeTab(tabs.flashcards)
 }
 
 function nextFlashcard() {
@@ -67,12 +76,21 @@ flashcard.addEventListener("click", () => {
 flashcard.addEventListener("animationend", () => {
     flashcard.classList.remove("appear")
 })
+saveToggle.addEventListener("click", () => {
+    if (saveToggle.classList.contains("off")) {
+        saveStatus.innerText = "Save"
+        saveToggle.classList.remove("off")
+        summaryMsg.innerHTML = "<p>Your Flashcards have been saved.</p>"
+        // save flashcards
+    } else {
+        saveStatus.innerText = "Don't save"
+        saveToggle.classList.add("off")
+        summaryMsg.innerHTML = `<img src="images/warning-icon.svg" alt="Warning" /><p>Your Flashcards are not saved.</p>`
+        // remove flashcards from storage
+    }
+})
 
 nextBtn.addEventListener("click", nextFlashcard)
-
-function showFlashcards() {
-    changeTab(tabs.error)
-}
 
 function showError() {
     changeTab(tabs.error)
