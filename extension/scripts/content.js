31248765734421f4d7eba12ab5f500ca61942858
filip-melka <melka.filip@proton.me>
2024-07-09 12:38:49 +0200

    feat: blur page

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -2,8 +2,33 @@ window.addEventListener("focus", function () {
     checkPage()
 })
 
+const body = document.getElementsByTagName("body")[0]
+body.style = "transition: filter 1s linear"
+
+function blurPage() {
+    body.style.filter = "blur(3px)"
+    body.style.overflowX = "hidden"
+    body.addEventListener("click", unblurPage)
+}
+
+function unblurPage() {
+    body.style.filter = "blur(0)"
+    body.removeEventListener("click", unblurPage)
+}
+
+// user opens a different tab
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState == "hidden") {
+        unblurPage()
+    }
+})
+
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.action === "getURL") {
+        // blur the page
+        blurPage()
+
+        // send back the URL
         sendResponse({
             url: window.location.href,
         })
