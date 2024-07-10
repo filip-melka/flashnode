e05c26c72b46b568cb476e4ef5be48fadf34cd07
filip-melka <melka.filip@proton.me>
2024-07-10 20:58:29 +0200

    feat: add dark mode

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import { Sidebar } from "@/components/sidebar"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 export const metadata: Metadata = {
     title: "Flashnode",
@@ -15,8 +17,16 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                {children}
-                <Sidebar />
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <ThemeToggle />
+                    {children}
+                    <Sidebar />
+                </ThemeProvider>
             </body>
         </html>
     )
