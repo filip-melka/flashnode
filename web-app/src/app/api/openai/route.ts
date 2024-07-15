d339e903e402a36a6ae45147ba432b2585b086b1
filip-melka <melka.filip@proton.me>
2024-07-15 11:53:22 +0200

    refactor: use actual response from openai

--- web-app/src/app/api/openai/route.ts
+++ web-app/src/app/api/openai/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
     try {
         const { markdown, title } = await req.json()
 
-        /* const res = await openai.chat.completions.create({
+        const res = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [
                 {
@@ -36,29 +36,7 @@ export async function POST(req: Request) {
         })
 
         const data = res.choices[0].message.content as string
-        const finishReason = res.choices[0].finish_reason */
-
-        const data = `Q: What is Test-Driven Development (TDD) and what are the main steps involved in the TDD cycle?
-A: Test-Driven Development (TDD) is a development process where tests are written before the actual code. The main steps involved in the TDD cycle are:
-1. Red: Write a test that fails.
-2. Green: Write the minimum code needed to make the test pass.
-3. Refactor: Improve the code while ensuring all tests still pass.
-
-Q: How can you set up Jest and React Testing Library for a React project?
-A: You can set up Jest and React Testing Library for a React project by either using Create React App (CRA) where Jest is included, or adding these dependencies using npm or yarn with 
-the following command:
-npm install --save-dev jest @testing-library/react @testing-library/jest-dom
-or
-yarn add --dev jest @testing-library/react @testing-library/jest-dom
-
-Q: What are some benefits of Test-Driven Development (TDD) in software development?
-A: Some benefits of Test-Driven Development (TDD) in software development include improved code quality, less debugging time, better design decisions, and serving as documentation for 
-code functionality.
-
-Q: What are some tips for effectively implementing Test-Driven Development (TDD) in React applications?
-A: Some tips for effectively implementing Test-Driven Development (TDD) in React applications include starting with small components, mocking external dependencies,`
-
-        const finishReason: string = "length"
+        const finishReason = res.choices[0].finish_reason
 
         const pairs = data.split("\n\n")
 
